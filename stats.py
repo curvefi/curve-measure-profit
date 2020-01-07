14d34d190882f63c86f2aa8c1f88ca24801271e5
Michael Egorov <michwill@gmail.com>
2020-01-07 15:56:17 +0100

    Stats every 5 s

--- stats.py
+++ stats.py
@@ -5,6 +5,8 @@ import config
 from web3.auto.infura import w3
 
 ###
+PRINT_FMT = r'[{0}] token_price: {1:.6f} | dai->usdc: {2:.6f} | usdc->dai: {3:.6f}'
+FILE_FMT = '{0}, {1:.6f}, {2:.6f}, {3:.6f}\n'
 swap = w3.eth.contract(abi=config.SWAP_ABI, address=config.SWAP_ADDRESS)
 
 
@@ -12,11 +14,15 @@ def get_info():
     virtual_price = swap.caller.get_virtual_price() / 10 ** 18
     dai2usdc = swap.caller.get_dy_underlying(0, 1, 10 ** 18) / 10 ** 6
     usdc2dai = swap.caller.get_dy_underlying(1, 0, 10 ** 6) / 10 ** 18
-    return virtual_price, dai2usdc, usdc2dai
+    return [virtual_price, dai2usdc, usdc2dai]
 
 
 if __name__ == '__main__':
     while True:
         results = get_info()
-        print(results)
+        t = int(time.time())
+        results = [t] + results
+        with open('swap-stats.csv', 'a') as f:
+            f.write(FILE_FMT.format(*results))
+        print(PRINT_FMT.format(*results))
         time.sleep(5)
