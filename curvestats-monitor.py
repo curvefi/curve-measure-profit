5acfbbdd4bceb4a7f00af1011d6b2766f0c08fd9
Michael Egorov <michwill@gmail.com>
2020-04-09 01:16:01 -0700

    Fetch new pools in the past if they were not present

--- curvestats-monitor.py
+++ curvestats-monitor.py
@@ -10,6 +10,7 @@ from functools import partial
 
 from curvestats.compound import CompoundPool
 from curvestats.y import YPool
+from curvestats.susd import SUSDPool
 
 MPOOL_SIZE = 40
 
@@ -17,7 +18,8 @@ pools = {
         'compound': (CompoundPool, ("0xA2B47E3D5c44877cca798226B7B8118F9BFb7A56", "0x845838DF265Dcd2c412A1Dc9e959c7d08537f8a2"), 9554041),
         'usdt': (CompoundPool, ("0x52EA46506B9CC5Ef470C5bf89f17Dc28bB35D85C", "0x9fC689CCaDa600B6DF723D9E47D84d76664a1F23"), 9456294),
         'y': (YPool, ("0x45F783CCE6B7FF23B2ab2D70e416cdb7D6055f51", "0xdF5e0e81Dff6FAF3A7e52BA697820c5e32D806A8"), 9476469),
-        'busd': (YPool, ("0x79a8C46DeA5aDa233ABaFFD40F3A0A2B1e5A4F27", "0x3B3Ac5386837Dc563660FB6a0937DFAa5924333B"), 9567296)
+        'busd': (YPool, ("0x79a8C46DeA5aDa233ABaFFD40F3A0A2B1e5A4F27", "0x3B3Ac5386837Dc563660FB6a0937DFAa5924333B"), 9567296),
+        'susd': (SUSDPool, ("0xeDf54bC005bc2Df0Cc6A675596e843D28b16A966", "0x2b645a6A426f22fB7954dC15E583e3737B8d1434"), 9636558)
 }
 start_blocks = {}
 
@@ -40,6 +42,20 @@ def int2uid(value):
     return int.to_bytes(value, 4, 'big')
 
 
+def pools_not_in_block(tx, b):
+    out = []
+    block = tx.get(int2uid(b))
+    if block:
+        block = json.loads(block)
+    if block:
+        for k in pools:
+            if k not in block:
+                out.append(k)
+    else:
+        out = list(pools)
+    return out
+
+
 mpool = Pool(MPOOL_SIZE)
 init_pools()
 
@@ -56,7 +72,7 @@ if __name__ == '__main__':
 
     # Initial data
     with db.begin(write=True) as tx:
-        if not tx.get(int2uid(0)):
+        if pools_not_in_block(tx, 0):
             tx.put(int2uid(0), json.dumps(
                         {k: {
                             'N': pool.N, 'decimals': pool.decimals,
@@ -76,9 +92,10 @@ if __name__ == '__main__':
         if current_block - start_block > MPOOL_SIZE:
             blocks = range(start_block, start_block + MPOOL_SIZE)
             with db.begin(write=True) as tx:
-                if not tx.get(int2uid(blocks[-1])):
+                pools_to_fetch = pools_not_in_block(tx, blocks[-1])
+                if pools_to_fetch:
                     stats = {}
-                    for p in pools:
+                    for p in pools_to_fetch:
                         if blocks[0] >= start_blocks[p]:
                             newstats = mpool.map(partial(fetch_stats, i=p), blocks)
                             for b, s in zip(blocks, newstats):
@@ -86,6 +103,10 @@ if __name__ == '__main__':
                                     stats[b] = {}
                                 stats[b][p] = s
                     for b, v in stats.items():
+                        block = tx.get(int2uid(b))
+                        if block:
+                            block = json.loads(block)
+                            v.update(block)
                         tx.put(int2uid(b), json.dumps(v).encode())
                     print('...', start_block)
                 else:
@@ -96,7 +117,7 @@ if __name__ == '__main__':
             if current_block > start_block:
                 for block in range(start_block, current_block):
                     with db.begin(write=True) as tx:
-                        if not tx.get(int2uid(block)):
+                        if pools_not_in_block(tx, block):
                             stats = {}
                             for p, pool in pools.items():
                                 if block >= start_blocks[p]:
