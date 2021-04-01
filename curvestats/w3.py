4297fdef8203c57bb02987dc04214bf346ad4aed
Michael Egorov <michwill@gmail.com>
2021-04-01 19:42:30 +0200

    Nah. Not using local node

--- curvestats/w3.py
+++ curvestats/w3.py
@@ -3,3 +3,4 @@ from web3 import Web3
 
 def w3():
     return Web3(Web3.HTTPProvider('https://rpc.fantom.network/'))
+    # return Web3(Web3.IPCProvider('/home/michwill/.lachesis/lachesis.ipc', timeout=60))
