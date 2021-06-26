1affa4448d4e87514e9ceb2b5c2b4e10804728f2
Michael Egorov <michwill@gmail.com>
2021-06-26 15:33:44 +0200

    Block

--- curvestats-monitor.py
+++ curvestats-monitor.py
@@ -70,7 +70,7 @@ if __name__ == '__main__':
 
     db = lmdb.open(DB_NAME, map_size=(2 ** 35))
 
-    start_block = 9464131
+    start_block = 10655000
     # start_block = w3.eth.getBlock('latest')['number'] - 1000
     print('Monitor started')
 
