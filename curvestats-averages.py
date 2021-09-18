abb7caf4f553e95330b543fae36b4f81693077df
Michael Egorov <michwill@gmail.com>
2021-09-18 11:52:04 +0200

    Averages script update

--- curvestats-averages.py
+++ curvestats-averages.py
@@ -33,12 +33,13 @@ if __name__ == "__main__":
             '2pool': [18, 6],
             'fusdt': [6, 18],
             'ren': [8, 8],
+            'tricrypto': [6, 8, 18],
     }
-    underlying_decimals = {'fusdt': [6, 18, 6]}
+    underlying_decimals = {'fusdt': [6, 18, 6], 'tricrypto': [6, 8, 18]}
     start_blocks = {}
     virtual_prices = []
     daily_volumes = defaultdict(float)
-    pools = ['2pool', 'fusdt', 'ren']
+    pools = ['2pool', 'fusdt', 'ren', 'tricrypto']
     andred_pools = set(['fusdt'])
     ctr = 0
     while True:
