33192d9534b59ee58fd56b82e95e96ea43e60bd3
Michael Egorov <michwill@gmail.com>
2021-10-10 12:32:10 +0200

    Harmony w3

--- curvestats/w3.py
+++ curvestats/w3.py
@@ -3,6 +3,6 @@ from web3 import Web3
 
 def w3():
     from web3.middleware import geth_poa_middleware
-    w3 = Web3(Web3.HTTPProvider('https://api.avax.network/ext/bc/C/rpc'))
+    w3 = Web3(Web3.HTTPProvider('https://api.harmony.one'))
     w3.middleware_onion.inject(geth_poa_middleware, layer=0)
     return w3
