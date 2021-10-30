233ce1530864e2a9be27e7a3092df0854b3c5c5a
Michael Egorov <michwill@gmail.com>
2021-10-30 21:41:11 +0200

    In-node modifications

--- curvestats/tricrypto.py
+++ curvestats/tricrypto.py
@@ -15,7 +15,7 @@ def load_abi(fname):
 
 
 class Pool:
-    @retry(Exception, delay=5, tries=5, backoff=2)
+    # @retry(Exception, delay=5, tries=5, backoff=2)
     def __init__(self, pool, token, w3=None):
         if not w3:
             from .w3 import w3 as our_w3
@@ -46,7 +46,7 @@ class Pool:
 
         self.underlying_coins = self.coins
 
-    @retry(Exception, delay=5, tries=5, backoff=2)
+    # @retry(Exception, delay=5, tries=5, backoff=2)
     def fetch_stats(self, block='latest'):
         full_block = self.w3.eth.getBlock(block)
         block = full_block['number']
--- curvestats/w3.py
+++ curvestats/w3.py
@@ -3,6 +3,8 @@ from web3 import Web3
 
 def w3():
     from web3.middleware import geth_poa_middleware
-    w3 = Web3(Web3.HTTPProvider('https://arbitrum-mainnet.infura.io/v3/fac98e56ea7e49608825dfc726fab703'))
+    # w3 = Web3(Web3.HTTPProvider('https://arbitrum-mainnet.infura.io/v3/fac98e56ea7e49608825dfc726fab703'))
+    w3 = Web3(Web3.HTTPProvider('https://arb-mainnet.g.alchemy.com/v2/D5O-iVrTJhd_coUFfsPFrcX5RFaJ0LIa'))
+    # w3 = Web3(Web3.HTTPProvider('https://arb1.arbitrum.io/rpc'))
     w3.middleware_onion.inject(geth_poa_middleware, layer=0)
     return w3
