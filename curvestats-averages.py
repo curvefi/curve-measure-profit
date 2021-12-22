1acc872bb388bed692557e4694e37507d36891dd
Michael Egorov <michwill@gmail.com>
2021-12-22 12:00:16 +0100

    XAUT

--- curvestats-averages.py
+++ curvestats-averages.py
@@ -36,14 +36,16 @@ if __name__ == "__main__":
             'eursusd': [6, 2],
             'crveth': [18, 18],
             'cvxeth': [18, 18],
+            'xautusd': [6, 18],
     }
-    underlying_decimals = {'tricrypto': [6, 8, 18], 'tricrypto2': [6, 8, 18], 'eurtusd': [6, 18, 6, 6], 'eursusd': [6, 2], 'crveth': [18, 18], 'cvxeth': [18, 18]}
+    underlying_decimals = {'tricrypto': [6, 8, 18], 'tricrypto2': [6, 8, 18], 'eurtusd': [6, 18, 6, 6],
+                           'eursusd': [6, 2], 'crveth': [18, 18], 'cvxeth': [18, 18], 'xautusd': [6, 18, 6, 6]}
     start_blocks = {}
     virtual_prices = []
     daily_volumes = defaultdict(float)
-    pools = ['tricrypto', 'tricrypto2', 'eurtusd', 'eursusd', 'crveth', 'cvxeth']
+    pools = ['tricrypto', 'tricrypto2', 'eurtusd', 'eursusd', 'crveth', 'cvxeth', 'xautusd']
     pool_names = {'tricrypto': 'tricrypto', 'tricrypto2': 'tricrypto2', 'eurtusd': 'eurtusd', 'eursusd': 'eursusd',
-                  'crveth': 'crveth', 'cvxeth': 'cvxeth'}
+                  'crveth': 'crveth', 'cvxeth': 'cvxeth', 'xautusd': 'xautusd'}
     ctr = 0
     while True:
         block = get_block(b)
--- curvestats-monitor.py
+++ curvestats-monitor.py
@@ -20,7 +20,8 @@ pools = {
         'eurtusd': (ForexPool, ('0x9838eCcC42659FA8AA7daF2aD134b53984c9427b', '0x3b6831c0077a1e44ED0a21841C3bC4dC11bCE833', '0xbEbc44782C7dB0a1A60Cb6fe97d0b483032FF1C7'), 13526620),
         'eursusd': (ForexPlain, ('0x98a7F18d4E56Cfe84E3D081B40001B3d5bD3eB8B', '0x3D229E1B4faab62F621eF2F6A610961f7BD7b23B'), 13530680),
         'crveth': (EthPool, ('0x8301AE4fc9c624d1D396cbDAa1ed877821D7C511', '0xEd4064f376cB8d68F770FB1Ff088a3d0F3FF5c4d'), 13676996),
-        'cvxeth': (EthPool, ('0xB576491F1E6e5E62f1d8F26062Ee822B40B0E0d4', '0x3A283D9c08E8b55966afb64C515f5143cf907611'), 13783426)
+        'cvxeth': (EthPool, ('0xB576491F1E6e5E62f1d8F26062Ee822B40B0E0d4', '0x3A283D9c08E8b55966afb64C515f5143cf907611'), 13783426),
+        'xautusd': (ForexPool, ('0xAdCFcf9894335dC340f6Cd182aFA45999F45Fc44', '0x8484673cA7BfF40F82B041916881aeA15ee84834', '0xbEbc44782C7dB0a1A60Cb6fe97d0b483032FF1C7'), 13854277),
 }
 start_blocks = {}
 
@@ -75,7 +76,7 @@ if __name__ == '__main__':
 
     db = lmdb.open(DB_NAME, map_size=(2 ** 35))
 
-    start_block = 13783426
+    start_block = 13854277
     print('Monitor started')
 
     # Initial data
